fb094ab0767be1aa61b902ed54b7c75e66971f73
alien <13373935140@163.com>
2022-06-19 23:16:44 +0800

    commit

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -1,43 +1,40 @@
 package kafka
 
-import (
-	"encoding/json"
-	"time"
+// import (
+// 	"encoding/json"
+// 	"time"
 
-	"github.com/Shopify/sarama"
-	cluster "github.com/bsm/sarama-cluster"
-	"github.com/oaago/component/logx"
-)
+// 	"github.com/Shopify/sarama"
+// 	cluster "github.com/bsm/sarama-cluster"
+// 	"github.com/oaago/component/logx"
+// )
 
-func NewConsumer(callback ConsumerCallback) {
-	consumer := ConsumerOptions
-	config := cluster.NewConfig()
-	config.Consumer.Return.Errors = true
-	config.Group.Return.Notifications = true
-	config.Consumer.Offsets.AutoCommit.Enable = true
-	config.Consumer.Offsets.AutoCommit.Interval = 1 * time.Second
-	config.Consumer.Offsets.CommitInterval = 1 * time.Second
-	config.Consumer.Offsets.Initial = sarama.OffsetOldest //初始从最新的offset开始
-	var err error
-	ConsumerGroup, err = cluster.NewConsumer(consumer.Nodes, consumer.GroupId, consumer.Topic, config)
-	defer ConsumerGroup.Close()
-	if err != nil {
-		logx.Logger.Info(err.Error())
-		return
-	}
-	go func() {
-		for err := range ConsumerGroup.Errors() {
-			logx.Logger.Error(err.Error())
-		}
-	}()
-	go func() {
-		for note := range ConsumerGroup.Notifications() {
-			content, _ := json.Marshal(note)
-			logx.Logger.Info(string(content))
-		}
-	}()
-	for msg := range ConsumerGroup.Messages() {
-		callback(msg, ConsumerGroup)
-	
-	}
-}
+// func NewConsumer(callback ConsumerCallback) {
+// 	consumer := ConsumerOptions
+// 	config := cluster.NewConfig()
+// 	config.Consumer.Return.Errors = true
+// 	config.Group.Return.Notifications = true
+// 	config.Consumer.Offsets.CommitInterval = 1 * time.Second
+// 	config.Consumer.Offsets.Initial = sarama.OffsetOldest //初始从最新的offset开始
+// 	var err error
+// 	ConsumerGroup, err = cluster.NewConsumer(consumer.Nodes, consumer.GroupId, consumer.Topic, config)
+// 	defer ConsumerGroup.Close()
+// 	if err != nil {
+// 		logx.Logger.Info(err.Error())
+// 		return
+// 	}
+// 	go func() {
+// 		for err := range ConsumerGroup.Errors() {
+// 			logx.Logger.Error(err.Error())
+// 		}
+// 	}()
+// 	go func() {
+// 		for note := range ConsumerGroup.Notifications() {
+// 			content, _ := json.Marshal(note)
+// 			logx.Logger.Info(string(content))
+// 		}
+// 	}()
+// 	for msg := range ConsumerGroup.Messages() {
+// 		callback(msg, ConsumerGroup)
+// 	}
+// }
